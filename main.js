9b3ca93dbf79de3b9b32369a65fda554668b7bec
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-09-30 10:55:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let score = 0;
 let target = document.getElementById("target");
 let scoreDisplay = document.getElementById("score");
 
-// Função para atualizar a posição da caixa
 function randomPosition() {
   let maxWidth = window.innerWidth - target.offsetWidth;
   let maxHeight = window.innerHeight - target.offsetHeight;
@@ -14,17 +13,13 @@ function randomPosition() {
   target.style.top = randomY + "px";
 }
 
-// Função para aumentar a pontuação
 function increaseScore() {
   score++;
   scoreDisplay.textContent = "Pontuação: " + score;
   randomPosition();
 }
 
-// Adiciona o evento de clique na caixa
-target.addEventListener("click", increaseScore);
-
-// Inicializa a posição da caixa
+target.addEventListener("c
 randomPosition();
 
 
